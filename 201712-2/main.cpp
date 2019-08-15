f0c245b2d16704fa14281202d8097998f7af9798
Hiko <johanhu2016@gmail.com>
2019-08-15 20:53:26 +0800

    201712-2: 30

---
+++ 201712-2/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    queue<int> a;
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i) {
+        a.push(i);
+    }
+    int num = 0;
+    while (n > 1) {
+        int t = a.front();
+        a.pop();
+        ++num;
+        if (num % k == 0 || num - k % 10 == 0) {
+            --n;
+        } else {
+            a.push(t);
+        }
+    }
+    cout << a.front() << endl;
+
+    return 0;
+}
