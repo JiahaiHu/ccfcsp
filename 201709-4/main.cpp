2ec646a2baef01fe7e2f0aabe361713eda619d17
Hiko <johanhu2016@gmail.com>
2019-08-19 13:22:37 +0800

    201709-4: 100

--- 201709-4/main.cpp
+++ 201709-4/main.cpp
@@ -10,6 +10,7 @@ vector<int> path;
 int num[MAX_N + 1] = {0};
 int _new = 0;
 
+// FIXME: runtime error
 void dfs(int n) {
     path.push_back(n);
     if (!vis[n]) {
@@ -33,6 +34,17 @@ void dfs(int n) {
     path.pop_back();
 }
 
+bool know[MAX_N + 1][MAX_N + 1];
+void DFS(int start, int v) {
+    vis[v] = true;
+    know[start][v] = know[v][start] = true;
+    for (int i : child[v]) {
+        if (!vis[i]) {
+            DFS(start, i);
+        }
+    }
+}
+
 int main() {
     int N, M;
     cin >> N >> M;
@@ -47,13 +59,18 @@ int main() {
         root[b] = false;
     }
     for (int i = 1; i <= N; ++i) {
-        if (root[i]) {
-            dfs(i);
-        }
+        // if (root[i]) {
+        //     dfs(i);
+        // }
+        fill(vis + 1, vis + N + 1, false);
+        DFS(i, i);
     }
     int sum = 0;
     for (int i = 1; i <= N; ++i) {
-        if (num[i] == N) {
+        // if (num[i] == N) {
+        //     sum++;
+        // }
+        if (count(know[i] + 1, know[i] + N + 1, true) == N) {
             sum++;
         }
     }
