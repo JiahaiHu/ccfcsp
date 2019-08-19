79d40655a3c5e96e09e4cd84210312d3ba0714d4
Hiko <johanhu2016@gmail.com>
2019-08-19 16:06:54 +0800

    201709-5: 50 (BIT)

--- 201709-5/main.cpp
+++ 201709-5/main.cpp
@@ -1,15 +1,32 @@
 #include <bits/stdc++.h>
+#define lowbit(i) (i&(-i))
 #define MAX_N 100000
 
 using namespace std;
 
 int num[MAX_N + 1];
+long long tree[MAX_N + 1] = {0};
+int N, M;
+
+void update(int x, int v) {
+    for (int i = x; i <= N; i += lowbit(i)) {
+        tree[i] += v;
+    }
+}
+
+long long get_sum(int x) {
+    long long sum = 0;
+    for (int i = x; i > 0; i -= lowbit(i)) {
+        sum += tree[i];
+    }
+    return sum;
+}
 
 int main() {
-    int N, M;
     cin >> N >> M;
     for (int i = 1; i <= N; ++i) {
         cin >> num[i];
+        update(i, num[i]);
     }
     while (M--) {
         int opt;
@@ -19,16 +36,14 @@ int main() {
             cin >> l >> r >> v;
             for (int i = l; i <= r; ++i) {
                 if (num[i] % v == 0) {
+                    int temp = num[i];
                     num[i] /= v;
+                    update(i, num[i] - temp);
                 }
             }
         } else {
             cin >> l >> r;
-            int sum = 0;
-            for (int i = l; i <= r; ++i) {
-                sum += num[i];
-            }
-            cout << sum << endl;
+            cout << get_sum(r) - get_sum(l - 1) << endl;
         }
     }
     return 0;
